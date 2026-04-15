c8e6b91b262934ba99516c4b94482473e4b581d2
Lukas Corbitt <movalley5@gmail.com>
2026-04-15 11:17:41 -0600

    Update homepage hero

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -9,13 +9,13 @@ export function Hero() {
       {/* Hero background image */}
       <div className="absolute inset-0 w-full h-full z-0">
         <Image
-          src="/hero-background.png"
+          src="/team-working.jpg"
           alt="Hero background"
           fill
           priority
           quality={75}
           sizes="100vw"
-          className="object-cover"
+          className="scale-x-[-1] object-cover"
           placeholder="blur"
           blurDataURL="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAIAAoDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAv/xAAhEAACAQMDBQAAAAAAAAAAAAABAgMABAUGIWGRkqGx0f/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAGhEAAgIDAAAAAAAAAAAAAAAAAAECEgMRkf/aAAwDAQACEQMRAD8AltJagyeH0AthI5xdrLcNM91BF5pX2HaH9bcfaSXWGaRmknyJckliyjqTzSlT54b6bk+h0R//2Q=="
           fetchPriority="high"
